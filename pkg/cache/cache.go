1d9e69e2055abfbfff3aff7c42420b37cf1ef4ec
skalmx <poplevinwork@gmail.com>
2023-04-26 20:16:36 +0300

    create structure for memorycaching and implements get and post in it

---
+++ pkg/cache/cache.go
@@ -0,0 +1,41 @@
+package cache
+
+import (
+	"L0/iternal/domain"
+	"errors"
+	"sync"
+)
+
+type MemoryCache struct {
+	sync.RWMutex
+	Cache map[string]domain.Order
+}
+
+func NewCache() *MemoryCache {
+	c := &MemoryCache{}
+	c.Cache = make(map[string]domain.Order)
+	
+	return c
+}
+
+func (c *MemoryCache) Set(order *domain.Order) error {
+	c.Lock()
+	defer c.Unlock()
+
+	c.Cache[order.OrderUID] = *order
+
+	return nil
+}
+
+func (c *MemoryCache) Get(key string) (domain.Order, error) {
+	c.RLock()
+	data, ex := c.Cache[key]
+	defer c.RUnlock()
+
+	if !ex {
+		return domain.Order{}, errors.New("no such element in mememory cache")
+	}
+
+	return data, nil
+}
+
